037a4a1526b4f125c35f13a65dcb7cd6fadb1f75
M4nu <manuolakeasesosa@gmail.com>
2021-06-15 20:32:05 -0300

    lint added

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,58 +1,72 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Item from './Item'
 
 class Game extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            list: [],
-            usedNumbers: [],
-        }
-        this.getData = this.getData.bind(this)
+  constructor(props) {
+	  super(props) 
+	  this.state = {
+      list : [],
+      usedNumbers : []
     }
+            this.getData = this.getData.bind(this);
+  }
 
-    async getData() {
-        for (let i = 0; i < 5; i++) {
-            try {
-                let num = parseInt(Math.random() * (200 - 1) + 1)
-                while (this.state.usedNumbers.includes(num)) {
-                    num = parseInt(Math.random() * (200 - 1) + 1)
-                }
-                this.state.usedNumbers.push(num)
-                console.log(num)
-                const response = await fetch(
-                    `https://pokeapi.co/api/v2/pokemon/${num}/`
-                )
-                const data = await response.json()
-                //console.log(data);
-                this.state.list.push([data.sprites.front_default, data.name])
-                this.setState((prevState) => ({
-                    list: prevState.list,
-                }))
-            } catch (err) {}
-        }
-    }
-    clear = () => {
-        this.setState({
-            list: [],
-        })
-    }
-    log = () => {
-        console.log(this.state)
-    }
-    render() {
-        const pokemons = this.state.list.map((arr) => (
-            <Item src={arr[0]} name={arr[1]} />
-        ))
-        return (
-            <div>
-                <h1>Game</h1>
-                <button onClick={this.getData}>X</button>
-                <button onClick={this.clear}>Clear</button>
-                <button onClick={this.log}>Log</button>
-                <br></br>
-                <div className="poke-container">{pokemons}</div>
-            </div>
+	async getData(n){
+		for (let i = 0; i < n; i++) {
+			try {
+			let num = parseInt(Math.random() * (800 - 1) + 1)
+
+			while (this.state.usedNumbers.includes(num)) {
+				num = parseInt(Math.random() * (800 - 1) + 1)
+			}
+
+			this.state.usedNumbers.push(num);
+		      	console.log(num);
+		      	const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}/`);
+		      	const data = await response.json();
+		      	console.log(data);
+	      		this.state.list.push([data.sprites.front_default, data.name]);
+			this.setState((prevState) => ({
+					list : prevState.list
+				}))
+			}catch (err) {}
+		}
+	}
+
+
+	clear = () => {
+		this.setState({
+			list : []
+		})
+	}
+
+	log = () => { 
+	console.log(this.state)
+	} 
+
+	componentDidMount(){
+		this.getData(5);
+	}
+
+	test(e) {
+		if(e.target.tagName != "DIV")
+		console.log(e.target.tagName);
+	}
+
+	render() {
+
+        	const pokemons = this.state.list.map((arr) => (
+            		<Item src={arr[0]} name={arr[1]} func={this.test}/>
+		));
+
+		return (
+			<div>
+			<h1>Game</h1>
+			<button onClick={this.clear}>Clear</button>
+	                <button onClick={this.log}>Log</button>
+                	<br></br>
+                	<div className="poke-container">{pokemons}</div>
+            	</div>
         )
     }
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -4,16 +4,22 @@ class Item extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            src: '',
-        }
+            src : ''
+	}
+
+	    this.log = this.log.bind(this)
     }
 
-    capitalize = (string) => string[0].toUpperCase() + string.slice(1)
+	capitalize = (string) => string[0].toUpperCase() + string.slice(1)
+
+	log(){
+		console.log(this.props.name);
+	}
 
     render() {
         return (
-            <div className="poke-item">
-                <img src={this.props.src} />
+            <div className="poke-item" onClick={this.props.func}>
+                <img src={this.props.src } alt=""/>
                 <h2>{this.capitalize(this.props.name)}</h2>
             </div>
         )
